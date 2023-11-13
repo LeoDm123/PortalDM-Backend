4d1a15bdb848947aa8d96c06663f97ee3fc5cf50
LeoDm123 <leo.meiners1@gmail.com>
2023-11-13 15:45:06 -0300

    editar  materiales

--- src/controllers/mats.controllers.js
+++ src/controllers/mats.controllers.js
@@ -56,7 +56,69 @@ const obtenerMats = async (req, res) => {
   }
 };
 
+const borrarMaterial = async (req, res) => {
+  const { MatID } = req.body;
+
+  try {
+    const material = await Materiales.findOne({ MatID });
+
+    if (!material) {
+      return res.json({
+        message: "Material no encontrado",
+      });
+    }
+
+    await material.deleteOne();
+
+    res.json({
+      message: "Material eliminado correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error en el servidor",
+    });
+  }
+};
+
+const editMaterial = async (req, res) => {
+  try {
+    const MatId = req.params.id;
+    const updatedMatData = req.body;
+
+    const updatedMat = await Materiales.findByIdAndUpdate(
+      MatId,
+      updatedMatData,
+      { new: true }
+    );
+
+    res.json(updatedMat);
+  } catch (error) {
+    console.error("Error al editar el material:", error);
+    res.status(500).json({ error: "Error al editar el material" });
+  }
+};
+
+const obtenerMatPorId = async (req, res) => {
+  try {
+    const matId = req.params.id;
+    const material = await Materiales.findById(matId);
+
+    if (!material) {
+      return res.status(404).json({ message: "Material no encontrado" });
+    }
+
+    return res.status(200).json(material);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Error interno del servidor" });
+  }
+};
+
 module.exports = {
   crearMaterial,
   obtenerMats,
+  borrarMaterial,
+  editMaterial,
+  obtenerMatPorId,
 };
--- src/routes/mats.js
+++ src/routes/mats.js
@@ -2,11 +2,17 @@ const express = require("express");
 const {
   crearMaterial,
   obtenerMats,
+  borrarMaterial,
+  editMaterial,
+  obtenerMatPorId,
 } = require("../controllers/mats.controllers");
 
 const routerMats = express.Router();
 
 routerMats.post("/crearMaterial", crearMaterial);
 routerMats.get("/obtenerMats", obtenerMats);
+routerMats.delete("/deleteMat/:id", borrarMaterial);
+routerMats.get("/obtenerMatPorID/:id", obtenerMatPorId);
+routerMats.put("/editMat/:id", editMaterial);
 
 module.exports = routerMats;
