e223da10adcabf361655d277c195663e9f5c2a2a
LeoDm123 <leo.meiners1@gmail.com>
2023-11-22 23:37:59 -0300

    avances recepcion de materiales

--- src/controllers/pedido.controllers.js
+++ src/controllers/pedido.controllers.js
@@ -44,4 +44,108 @@ const obtenerPedidos = async (req, res) => {
   }
 };
 
-module.exports = { crearPedido, obtenerPedidos };
+const deletePedido = async (req, res) => {
+  try {
+    const deletedPedido = await Pedidos.findByIdAndDelete(req.params.id);
+
+    if (!deletedPedido) {
+      return res.status(404).json({ message: "Pedido no encontrado" });
+    }
+
+    return res.status(200).json({ message: "Pedido eliminado exitosamente" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const obtenerPedidoPorId = async (req, res) => {
+  try {
+    const pedidoId = req.params.id;
+    const pedido = await Pedidos.findById(pedidoId);
+
+    if (!pedido) {
+      return res.status(404).json({ message: "Pedido no encontrado" });
+    }
+
+    return res.status(200).json(pedido);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Error interno del servidor" });
+  }
+};
+
+const recibirPedido = async (req, res) => {
+  try {
+    const pedidoId = req.params.pedidoId;
+    const codigoMat = req.params.codigoMat;
+    const { CantRecibida, FechaRecep, NroRemito } = req.body;
+
+    const pedido = await Pedidos.findOne({ _id: pedidoId });
+
+    if (!pedido) {
+      console.log("Pedido no encontrado");
+      return res.status(404).json({ message: "Pedido no encontrado" });
+    }
+
+    const materialEncontrado = pedido.Materiales.find(
+      (material) => material.Codigo === codigoMat
+    );
+
+    if (!materialEncontrado) {
+      console.log("Material no encontrado en el pedido");
+      return res
+        .status(404)
+        .json({ message: "Material no encontrado en el pedido" });
+    }
+
+    materialEncontrado.recepciones = materialEncontrado.recepciones || [];
+    materialEncontrado.recepciones.push(req.body);
+
+    await pedido.save();
+
+    return res.status(200).json(materialEncontrado);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Error interno del servidor" });
+  }
+};
+
+const obtenerMaterialPorCodigo = async (req, res) => {
+  try {
+    const pedidoId = req.params.pedidoId;
+    const codigoMat = req.params.codigoMat;
+
+    const pedido = await Pedidos.findOne({ _id: pedidoId });
+
+    if (!pedido) {
+      console.log("Pedido no encontrado");
+      return res.status(404).json({ message: "Pedido no encontrado" });
+    }
+
+    const materialEncontrado = pedido.Materiales.find(
+      (material) => material.Codigo === codigoMat
+    );
+
+    if (!materialEncontrado) {
+      console.log("Material no encontrado en el pedido");
+      return res
+        .status(404)
+        .json({ message: "Material no encontrado en el pedido" });
+    }
+
+    return res.status(200).json(materialEncontrado);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Error interno del servidor" });
+  }
+};
+
+module.exports = {
+  crearPedido,
+  obtenerPedidos,
+  deletePedido,
+  recibirPedido,
+  obtenerPedidoPorId,
+  obtenerMaterialPorCodigo,
+};
--- src/routes/pedido.js
+++ src/routes/pedido.js
@@ -2,11 +2,22 @@ const express = require("express");
 const {
   crearPedido,
   obtenerPedidos,
+  deletePedido,
+  recibirPedido,
+  obtenerPedidoPorId,
+  obtenerMaterialPorCodigo,
 } = require("../controllers/pedido.controllers");
 
 const routerPedido = express.Router();
 
 routerPedido.post("/crearPedido", crearPedido);
 routerPedido.get("/obtenerPedidos", obtenerPedidos);
+routerPedido.delete("/deletePedido/:id", deletePedido);
+routerPedido.put("/recibirPedido/:pedidoId/:codigoMat", recibirPedido);
+routerPedido.get("/obtenerPedidoPorId/:id", obtenerPedidoPorId);
+routerPedido.get(
+  "/obtenerMaterialPorCodigo/:pedidoId/:codigoMat",
+  obtenerMaterialPorCodigo
+);
 
 module.exports = routerPedido;
