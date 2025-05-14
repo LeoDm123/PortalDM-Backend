16ddec5120ab499567676b72f45790daa25b23e9
LeoDm123 <leo.meiners1@gmail.com>
2025-05-14 16:38:46 -0300

    fetch clientes activos

--- src/routes/clients.js
+++ src/routes/clients.js
@@ -18,5 +18,6 @@ routerClients.delete("/deleteCliente/:id", DeleteCliente);
 routerClients.put("/editCliente/:id", EditCliente);
 routerClients.get("/obtenerClientePorId/:id", obtenerClientePorId);
 routerClients.get("/obtenerClientesActivos", obtenerClientesActivos);
+//hola
 
 module.exports = routerClients;
