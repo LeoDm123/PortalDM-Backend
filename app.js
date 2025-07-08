a415c5a0f2be49d1daa49ac98fc1c32efd3a1321
LeoDm123 <leo.meiners1@gmail.com>
2025-07-07 22:57:11 -0300

    cambio en CORS

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ const { dbConnection } = require("./src/database/config");
 const app = express();
 const cors = require("cors");
 
+const allowedOrigins = [process.env.DEV_DOM, process.env.BACK_API];
+
 require("dotenv").config();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 dbConnection();
 
